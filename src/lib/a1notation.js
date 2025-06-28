42b635741f4ea07a2c1eab7d0ee4f2e47feceb9a
zionhigt <larcher501@gmail.com>
2025-06-28 17:29:03 +0200

    A1 LIB

---
+++ src/lib/a1notation.js
@@ -0,0 +1,69 @@
+/** alpha refrence the math base 26 */
+
+const base = 26;
+function dec2alpha(n) {
+    let chars = []
+    let q = n;
+    while(q > 0) {
+        chars.push((q - 1) % base);
+        q = Math.floor(q / base);
+    }
+    chars = chars.reverse();
+    if (chars.length === 0) chars.push(0);
+    return chars;
+}
+
+function charAlpha(n) {
+    return String.fromCharCode(
+        "A".charCodeAt() + n
+    )
+}
+
+function nAlpha(a) {
+    return a.toUpperCase().charCodeAt() - "A".charCodeAt();
+}
+
+export function vect2a1(vect) {
+    if (vect.length != 2) throw new Error("Vector must have two axes [row, col]")
+    const [i, j] = vect;
+    return `${dec2alpha(j).map(charAlpha).join("")}${i}`;
+
+}
+
+export function parseNotation(notation) {
+    let cursor = 0;
+    const result = [[], []];
+    while (cursor < notation.length) {
+        const token = notation[cursor ++].toUpperCase();
+        if (token >= "A" && token <= "Z") {
+            result[0].push(token);
+        } else if (token >= "0" && token <= "9") {
+            result[1].push(token);
+        } else {
+            throw new Error("Bad token found : " + token);
+        }
+    }
+    return result.map(function(item) {
+        return item.join("");
+    });
+}
+
+export function a12vect(notation) {
+    const [a, n] = parseNotation(notation);
+    return [
+        Number.parseInt(n),
+        alpha2dec(a)
+    ]
+}
+
+export function alpha2dec(a) {
+    const sign = [];
+    for (let c of a.split("").reverse()) {
+        sign.push(nAlpha(c))
+    }
+
+    const r = sign.reduce(function(acc, item, i) {
+        return acc += (((item + 1) % base) * Math.pow(base, i));
+    }, 0)
+    return r;
+}
